f65f52c2ed532b949711d7ea59c468701d080b71
onejemee <>
2021-06-06 21:40:36 +0000

    Added template literals, cleaned up code, hopefully output is good

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,36 +8,43 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["1) Who was the first American woman in space?", "2) True or false: 5 kilometer == 5000 meters?", 	
+"3) (5 + 3)/2 * 10 = ?", "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", 	
+"5) What is the minimum crew size for the ISS?"];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"]
+let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
   candidateName = input.question("What is your name?")
-  console.log(`Hello, ${candidateName}`)
+  console.log(`Candidate Name: ${candidateName}`)
 }
 
-function askQuestion() {
+function askQuestion(questions) {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  this.candidateAnswer = input.question(question)
-
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswers[i] = input.question(questions[i]);
+  }
+   return candidateAnswers; 
 }
 
 
-function gradeQuiz(candidateAnswer) {
-
+function gradeQuiz(candidateAnswers) {
+  let grade = 0;
+  console.log(`Candidate Name: ${candidateName}`);
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  console.log(this.candidateAnswer);
-  if (this.candidateAnswer == correctAnswer) {
-    console.log("Correct!")
-  }
-  else {
-    console.log("Incorrect. Please restart quiz. ")
-  }
-
-
-  let grade;
+  for (let i = 0; i < correctAnswers.length; i++) {
+    console.log(`${questions[i]}`)
+    console.log(`Your Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}`)
+    if (candidateAnswers[i].toLowerCase()==correctAnswers[i].toLowerCase()) {
+      grade = grade + 1;
+    }
+  };
+  console.log(`>>> Overall Grade: ${(grade/questions.length)*100} (${grade} of 5 responses correct) <<<`)
+  if (grade < 4) {
+    console.log(">>> Status: FAILED <<<")
+  } else console.log(">>> Status: PASSED <<<")
+   
   
 
   return grade;
@@ -46,9 +53,8 @@ function gradeQuiz(candidateAnswer) {
 function runProgram() {
   askForName(); 
   // TODO 1.1c: Ask for candidate's name //
-  askQuestion(); 
-  gradeQuiz(this.candidateAnswer);
-  
+  let candidateAnswers_1 = askQuestion(this.questions); 
+  gradeQuiz(candidateAnswers_1);
 }
 
 // Don't write any code below this line //

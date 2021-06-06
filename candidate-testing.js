c1569a3ec59888f96efbad7a3e16b7cbae6546ba
onejemee <>
2021-06-06 23:53:10 +0000

    moved numbers

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,9 +8,9 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["1) Who was the first American woman in space? ", "2) True or false: 5 kilometer == 5000 meters? ", 	
-"3) (5 + 3)/2 * 10 = ? ", "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 	
-"5) What is the minimum crew size for the ISS? "];
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", 	
+"(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 	
+"What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"]
 let candidateAnswers = [];
 
@@ -34,7 +34,7 @@ function gradeQuiz(candidateAnswers) {
   console.log(`Candidate Name: ${candidateName}`);
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
   for (let i = 0; i < correctAnswers.length; i++) {
-    console.log(`${questions[i]}`)
+    console.log(`${i+1}) ${questions[i]}`)
     console.log(`Your Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}`)
     if (candidateAnswers[i].toLowerCase()==correctAnswers[i].toLowerCase()) {
       grade = grade + 1;
